d4fe0e8703688b7ffaa44ca4364eed73277eb10e
Arthurnicolaul <lontra.ab@gmail.com>
2023-06-05 23:12:36 -0300

    first commit

---
+++ src/test/java/br/com/controledegastos/controlegastos/ControlegastosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.controledegastos.controlegastos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlegastosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
